7b6a85172c9df158eae57a650e3aa0fe7c778663
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2025-09-27 08:31:07 -0300

    ADD: Adding a screen to view a task

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import NewTask from "../pages/NewTesk/NewTask";
+import TaskDetail from "../pages/TaskDetail/TaskDetail";
 
 export default function AppRoutes() {
 
@@ -9,6 +10,7 @@ export default function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/task/new" element={<NewTask/>}/>
+                <Route path="/task/:id" element={<TaskDetail/>}/>
             </Routes>
         </BrowserRouter>
     );
