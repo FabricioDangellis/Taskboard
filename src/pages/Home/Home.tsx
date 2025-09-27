58d7b9701f887159179dce54234008d76ca44266
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2025-09-27 14:52:33 -0300

    FIX: Improvements to the Home page interface

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Task from "../../components/Task";
 import { useTask } from "../../context/TaskContext";
-import { Plus } from "lucide-react";
+import { List, Plus } from "lucide-react";
 
 export default function Home() {
   const { tasks } = useTask();
@@ -12,40 +12,55 @@ export default function Home() {
   });
 
   return (
-    <div className="w-full min-h-screen flex justify-center items-center bg-gray-100">
-      <div className="w-2/3 min-w-[360px] bg-white shadow-lg rounded-xl overflow-hidden">
+    <div className="w-full min-h-screen relative py-6 px-4 bg-off-white">
 
-        {/* Header */}
-        <header className="w-full bg-country-green py-6 flex justify-center items-center">
-          <h1 className="font-bold text-3xl text-white">TaskBoard</h1>
-        </header>
+      <header className="h-1/6 flex items-center gap-2 mb-6">
+        <List className="w-8 h-8 text-sky-blue" />
+        <h1 className="font-bold text-3xl text-gray-800">TaskBoard</h1>
+      </header>
 
-        {/* Conteúdo */}
-        <section className="p-6">
-          <h2 className="text-2xl font-semibold mb-4">My Tasks</h2>
+      <main className="flex justify-center mt-10 mb-10">
 
-          {tasks.length === 0 ? (
-            <p className="text-gray-500 text-center">
-              Nenhuma tarefa cadastrada.
-            </p>
-          ) : (
-            <div className="space-y-4">
-              {priorityTasks.map((task) => (
-                <Task key={task.id} task={task} />
-              ))}
+        <div className="w-2/3 min-w-[360px] relative bg-white shadow-lg rounded-xl overflow-hidden flex flex-col">
+
+          <header className="w-full bg-sky-blue py-6 flex justify-center items-center">
+            <h1 className="font-bold text-3xl text-white">TaskBoard</h1>
+          </header>
+
+          <section className="p-6 pb-28">
+            <div className="mb-5">
+              <h2 className="text-2xl font-semibold mb-0.5">My Tasks</h2>
+              <span className="text-gray-500">{tasks.length} tasks</span>
+
             </div>
-          )}
-        </section>
 
-        <div className="flex justify-end">
-          <div className="w-10 h-10 m-3 bg-country-green rounded-xl">
+            {tasks.length === 0 ? (
+              <p className="text-gray-500 text-center">
+                Nenhuma tarefa cadastrada.
+              </p>
+            ) : (
+              <div className="space-y-4">
+                {priorityTasks.map((task) => (
+                  <Task key={task.id} task={task} />
+                ))}
+              </div>
+            )}
+          </section>
+
+          <div className="absolute bottom-6 right-6 w-10 h-10 m-3 bg-sky-blue rounded-xl shadow hover:bg-sky-light-blue transition">
             <Link className="w-10 h-10 flex items-center justify-center" to={"/task/new"}>
               <Plus color="white" />
             </Link>
           </div>
+
         </div>
 
-      </div>
+      </main>
+
+      <footer className="absolute bottom-0 left-0 w-full my-4 mt-6 text-gray-500 text-sm text-center">
+        2025 - Desenvolvido por <Link to="https://github.com/FabricioDangellis" target="_blank" rel="noopener noreferrer">@Fabrício D’angellis</Link>
+      </footer>
+
     </div>
   );
 }
